e4d26eb1e7e47ef9d1d6965f28f6715cc8581531
Quatters <vladnfs3@gmail.com>
2022-03-16 19:30:29 +1000

    using DB_URI env var if exists

--- api/data/db.config.js
+++ api/data/db.config.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-const DB_URI = `mongodb://localhost:27017/${
-  process.env.NODE_ENV === 'test' ? 'vk-next-test' : 'vk-next'
-}`;
+const DB_URI =
+  process.env.DB_URI ||
+  `mongodb://localhost:27017/${
+    process.env.NODE_ENV === 'test' ? 'vk-next-test' : 'vk-next'
+  }`;
 
 mongoose.connect(DB_URI, {
   useNewUrlParser: true,
